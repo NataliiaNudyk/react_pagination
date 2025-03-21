2c36ffe42f16b5d49c48e0bac28702d5534af286
Nataliia Nudyk <nataliianudyk@gmail.com>
2025-03-21 16:31:31 +0200

    fix solution

--- src/App.tsx
+++ src/App.tsx
@@ -39,9 +39,7 @@ export const App: React.FC = () => {
             value={defaultItemValue}
           >
             <option value="3">3</option>
-            <option selected value="5">
-              5
-            </option>
+            <option value="5">5</option>
             <option value="10">10</option>
             <option value="20">20</option>
           </select>
